6f8700c5d4def0e5cc3fb26ab0af2d9291078416
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-23 07:21:35 +0100

    Setup Linters

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,8 +1,7 @@
 class GreetingsController < ApplicationController
-    def index
-      @greetings = Greeting.all
-      @random_greet = @greetings[rand(Greeting.all.length)]
-      render json: { greeting: @random_greet }.to_json
-    end
+  def index
+    @greetings = Greeting.all
+    @random_greet = @greetings[rand(Greeting.all.length)]
+    render json: { greeting: @random_greet }.to_json
   end
-  
+end
